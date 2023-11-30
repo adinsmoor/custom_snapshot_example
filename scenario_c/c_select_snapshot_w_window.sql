9e9a2cb8cc3219115e802ef6a54d3bf6e1938b30
adinsmoor <108648677+adinsmoor@users.noreply.github.com>
2023-11-30 18:13:30 +1100

    Update c_select_snapshot_w_window.sql

--- scenario_c/c_select_snapshot_w_window.sql
+++ scenario_c/c_select_snapshot_w_window.sql
@@ -1,12 +1,10 @@
--- select * from {{ ref('c_member_sub_snapshot') }}
-
 with final as (
 select 
     digipass_id
     , status
     , dbt_valid_from
     , dbt_valid_to
-    , last_value(address) over (partition by digipass_id order by updated_at asc) as address
+    , last_value(address) over (partition by digipass_id, status order by dbt_valid_from asc) as address
 from {{ ref('c_member_sub_snapshot') }}
 -- order by digipass_id, updated_at
 )
@@ -15,8 +13,36 @@ select
     digipass_id
     , status
     , address
-    , max(dbt_valid_from)
+    , min(dbt_valid_from)
     , max(dbt_valid_to)
 from final 
 group by 1,2,3
 order by 1,4
+
+
+
+
+
+-- archived version below
+-- select * from {{ ref('c_member_sub_snapshot') }}
+
+-- with final as (
+-- select 
+--     digipass_id
+--     , status
+--     , dbt_valid_from
+--     , dbt_valid_to
+--     , last_value(address) over (partition by digipass_id order by updated_at asc) as address
+-- from {{ ref('c_member_sub_snapshot') }}
+-- -- order by digipass_id, updated_at
+-- )
+
+-- select 
+--     digipass_id
+--     , status
+--     , address
+--     , max(dbt_valid_from)
+--     , max(dbt_valid_to)
+-- from final 
+-- group by 1,2,3
+-- order by 1,4
